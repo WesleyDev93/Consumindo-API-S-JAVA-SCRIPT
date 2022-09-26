f4c3d14c0894b2fcdc388096c1a3e8a2326ab59d
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-09-26 11:16:21 -0300

    usando .finally para mensagem de processamento

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 var consultaCep = fetch ('https:viacep.com.br/ws/01001000/json/')
-.then (resposta => resposta.json()).then(r => console.log (r));
+.then (resposta => resposta.json()).then(r => {
+          
+      if (r.erro) {
+        throw Error('Este cep não existe !')
+      } else
+
+ console.log (r)
+})
+
+.catch (erro => console.log (erro))
+.finally (mensagem => console.log ('Processamento concluido'));
 
 console.log (consultaCep);
