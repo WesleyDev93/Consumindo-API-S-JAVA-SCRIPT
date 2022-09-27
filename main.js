adaa6efef44712ce217b0a4c02e23a3056a12ad6
Wesley Oliveira Lima Da Silva <wesleyoliveiralimadasilva@gmail.com>
2022-09-27 12:06:12 -0300

    usando async await ao inves de then, para evitar callback hell

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-var consultaCep = fetch ('https:viacep.com.br/ws/01001000/json/')
-.then (resposta => resposta.json()).then(r => {
+async function buscaEndereco () {
+    var consultaCep = await fetch ('https:viacep.com.br/ws/01001000/json/');
+    var consultaCepConvertida = await consultaCep.json ();
+
+
+
+    console.log (consultaCepConvertida);
+}
+
+
+
+
+buscaEndereco ();
+
+
+
+
+
+
+
+
+//.then (resposta => resposta.json()).then(r => {
           
-      if (r.erro) {
-        throw Error('Este cep não existe !')
-      } else
+     // if (r.erro) {
+      //  throw Error('Este cep não existe !')
+     // } else
 
- console.log (r)
-})
+ //console.log (r)
+//})
 
-.catch (erro => console.log (erro))
-.finally (mensagem => console.log ('Processamento concluido'));
-
-console.log (consultaCep);
+//.catch (erro => console.log (erro))
+//.finally (mensagem => console.log ('Processamento concluido'));
+//console.log (consultaCep);
